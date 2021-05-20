9d1b44155d2e214bec42feb25c6511139d178605
MarianaGrandinetti <mariana.grandinetti@reserv.com.ar>
2021-05-20 17:28:41 -0300

    commit clase

---
+++ src/Persona.java
@@ -0,0 +1,30 @@
+
+public class Persona {
+	String nombre;
+	String apellido;
+	int dni;
+	int edad; 
+	int altura;
+	int peso;
+	String estado;
+	
+
+	
+	public Persona(String nombre,String apellido,int dni,int edad,int altura,int peso,String estado) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.edad = edad;
+		this.altura = altura; 
+		this.peso = peso; 
+		this.estado = estado;
+		
+	}
+
+	public void Caminar () {}
+	public void Sentarse () {}
+	public void Pararse () {}
+	public void Cumplir () {}
+	public void Engordar () {}
+	
+}
---
+++ src/Principal.java
@@ -0,0 +1,8 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Persona persona1 = new Persona("Mariana","grandi", 29345678,39,168,89,"parado");
+		
+	}
+}
