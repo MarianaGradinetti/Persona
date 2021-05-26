53dde71c4e26eecb6ec9a2549cdcc247442867c3
MarianaGrandinetti <mariana.grandinetti@reserv.com.ar>
2021-05-26 16:02:46 -0300

    nuevo

--- src/Persona.java
+++ src/Persona.java
@@ -36,5 +36,61 @@ public class Persona {
 	public void Engordar () {
 		
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
 	
 }
--- src/Principal.java
+++ src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Persona persona1 = new Persona("Mariana","grandi", 29345678,39,168,89,"parado");
-		system.out.println.persona1.Mariana;
+		system.out.println.persona1.("Mariana");
 		
 		
 	}
